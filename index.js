a00af95cfda1b26a304c5c7be957e48b3c865fdf
ritg2 <ritking2@gmail.com>
2023-05-04 14:10:37 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const scrollBtn = document.querySelector(".scroll-btn");
+
+window.onscroll = function () {
+  scrollFunction();
+};
+
+let position =  300
+
+function scrollFunction() {
+  if (document.body.scrollTop > position || document.documentElement.scrollTop > position) {
+    scrollBtn.style.display = "block";
+  } else {
+    scrollBtn.style.display = "none";
+  }
+}
+
+scrollBtn.addEventListener("click", () => {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+});
